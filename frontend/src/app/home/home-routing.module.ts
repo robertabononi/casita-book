85b09e72a0265a2735a1956e03a776641df38617
robertabononi <roberta.bononi16@gmail.com>
2021-08-12 19:34:39 -0300

    feat: create NewUserComponent and add it to the routes

--- frontend/src/app/home/home-routing.module.ts
+++ frontend/src/app/home/home-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
-      { path: '', component: LoginComponent }
+      { path: '', component: LoginComponent },
+      { path: 'novo-usuario', component: NewUserComponent }
     ]
   },
 ];
