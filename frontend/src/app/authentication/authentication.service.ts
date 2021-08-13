1c0b287348d00e4666232d2bfcdb4485d26b0f47
robertabononi <roberta.bononi16@gmail.com>
2021-08-13 13:09:57 -0300

    chore: alter authentication service to save token on user service

--- frontend/src/app/authentication/authentication.service.ts
+++ frontend/src/app/authentication/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { UserService } from './user/user.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,24 @@ export class AuthenticationService {
 
   urlApi = 'http://localhost:3000/user/login'
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {}
 
-  authenticate(user: string, password: string): Observable<any> {
-    return this.httpClient.post(this.urlApi, {
-      userName: user,
-      // tslint:disable-next-line: object-literal-shorthand
-      password: password
-    })
+  authenticate(user: string, password: string): Observable<HttpResponse<any>> {
+    return this.httpClient.post(this.urlApi,
+      {
+        userName: user,
+        // tslint:disable-next-line: object-literal-shorthand
+        password: password
+      },
+      { observe: 'response' }
+    ).pipe(
+      tap((res) => {
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.userService.saveToken(authToken);
+      })
+    )
   }
 }
